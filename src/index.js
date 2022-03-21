8d7618b67d4243b41a43daed6baf5aa5f6eb0f99
rabeb-bb <rabeb.bahry@gmail.com>
2022-03-21 21:34:01 +0100

    fix bug

--- src/index.js
+++ src/index.js
@@ -1,13 +1,9 @@
 const RSDiff = (arr1, arr2) => {
   // compare function
   const isSame = (a, b) => a.msgid === b.msgid;
-  const isDiff = (a, b) => a.msgid !== b.msgid;
-
   //extract differences
   const Diff = (one, two, compareFunction) =>
-    two.filter((item2) => {
-      !one.some((item1) => compareFunction(item2, item1));
-    });
+    two.filter((item2) => !one.some((item1) => compareFunction(item2, item1)));
 
   const Similar = (one, two, compareFunction) =>
     two.filter((item2) => one.some((item1) => compareFunction(item2, item1)));
@@ -24,3 +20,71 @@ const RSDiff = (arr1, arr2) => {
 };
 
 module.exports = RSDiff;
+
+// const arr1 = [
+//   {
+//     msgid: "TODO List",
+//     msgctxt: null,
+//     references: ["/app/modules/views"],
+//     msgid_plural: null,
+//     msgstr: ["TODO List"],
+//     comments: [],
+//     extractedComments: ["Please don't change the name of the application"],
+//     flags: {},
+//     obsolete: false,
+//     nplurals: 2,
+//   },
+//   {
+//     msgid: "Just one user online",
+//     msgctxt: null,
+//     references: ["/app/modules/views"],
+//     msgid_plural: "There are %d users online",
+//     msgstr: ["Un seul utilisateur en ligne", "Il y a %d utilisateurs en ligne"],
+//     comments: [],
+//     extractedComments: [],
+//     flags: {},
+//     obsolete: false,
+//     nplurals: 2,
+//   },
+// ];
+// const arr2 = [
+//   {
+//     msgid: "TODO List",
+//     msgctxt: null,
+//     references: ["/app/modules/views"],
+//     msgid_plural: null,
+//     msgstr: ["TODO List"],
+//     comments: [],
+//     extractedComments: ["Please don't change the name of the application"],
+//     flags: {},
+//     obsolete: false,
+//     nplurals: 2,
+//   },
+//   {
+//     msgid: "Enter your credentials below to login",
+//     msgctxt: null,
+//     references: ["/app/modules/views"],
+//     msgid_plural: null,
+//     msgstr: ["Entrez vos identifiants ci-dessous pour vous connecter"],
+//     comments: [],
+//     extractedComments: [],
+//     flags: {},
+//     obsolete: false,
+//     nplurals: 2,
+//   },
+//   {
+//     msgid: "Password",
+//     msgctxt: null,
+//     references: ["/app/modules/views"],
+//     msgid_plural: null,
+//     msgstr: ["Mot de passe"],
+//     comments: [],
+//     extractedComments: [],
+//     flags: {},
+//     obsolete: false,
+//     nplurals: 2,
+//   },
+// ];
+
+// const result = RSDiff(arr1, arr2);
+// console.log(result);
